8cc87fbd7675e72dad6b7da9a23845ee19235509
e <1421805850@qq.com>
2019-03-07 10:37:39 +0800

    音乐列表

--- imooc-videos-dev-mini-api/src/main/java/com/lyj/controller/BgmController.java
+++ imooc-videos-dev-mini-api/src/main/java/com/lyj/controller/BgmController.java
@@ -14,7 +14,7 @@ import com.lyj.utils.IMoocJSONResult;
 /**
  * @author 作者 lyj
  * @version 创建时间：2019年3月5日 下午5:31:52
- * 类说明
+ * 音乐列表
  */
 @RestController
 @Api(value = "背景音乐业务的接口", tags = {"背景音乐业务的controller"})
@@ -24,6 +24,10 @@ public class BgmController {
 	@Autowired
 	private BgmService bgmService;
 	
+	/**
+	 * 音乐列表
+	 * @return
+	 */
 	@ApiOperation(value = "获取背景音乐列表", notes = "用户上传头像的接口")
 	@PostMapping("/list")
 	public IMoocJSONResult bmgList() {
